18eb11164c074380f73e250a73fe392ec536b21e
Naitik Shah <n@daaku.org>
2019-07-03 15:20:56 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,84 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    jsx: true,
+    useJSXTextNode: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  plugins: [
+    'prettier',
+    'promise',
+    '@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'class-methods-use-this': 'off',
+    'default-case': 'off',
+    'import/named': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'jsx-a11y/label-has-for': 'off',
+    'no-case-declarations': 'off',
+    'no-console': 'off',
+    'no-continue': 'off',
+    'no-param-reassign': 'off',
+    'no-plusplus': 'off',
+    'no-undef': 'off',
+    'object-curly-newline': 'off',
+    'prettier/prettier': 'error',
+    'promise/always-return': 'error',
+    'promise/avoid-new': 'off',
+    'promise/catch-or-return': 'error',
+    'promise/no-callback-in-promise': 'error',
+    'promise/no-native': 'off',
+    'promise/no-nesting': 'error',
+    'promise/no-new-statics': 'error',
+    'promise/no-promise-in-callback': 'error',
+    'promise/no-return-in-finally': 'error',
+    'promise/no-return-wrap': 'error',
+    'promise/param-names': 'error',
+    'promise/valid-params': 'error',
+    'react/prop-types': 'off',
+    'strict': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        'selector': 'ForInStatement',
+        'message': 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.'
+      },
+      {
+        'selector': 'LabeledStatement',
+        'message': 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.'
+      },
+      {
+        'selector': 'WithStatement',
+        'message': '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
+      }
+    ],
+  },
+  overrides: {
+    files: ['*/__tests__/*'],
+    rules: {
+      '@typescript-eslint/no-object-literal-type-assertion': 0,
+      '@typescript-eslint/no-explicit-any': 0,
+    },
+  },
+};
