447144e70d77fd5bc4095ddfe6c8c95c876307b3
Naitik Shah <n@daaku.org>
2019-07-12 23:40:46 +0400

    disable @typescript-eslint/no-parameter-properties

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/indent': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'class-methods-use-this': 'off',
     'default-case': 'off',
